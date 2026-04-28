6df3d11b3b209e66cbf55f777277f6270328998e
Sanzhar <sanzarbajzak45@gmail.com>
2026-04-28 08:03:40 +0500

    demo3

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.DriveMotor;
@@ -19,35 +17,27 @@ public class Robot extends TimedRobot {
         ds.enable();
 
         m_robotContainer = new RobotContainer();
-
-        if (RobotContainer.autoChooser == null) {
-            RobotContainer.autoChooser = new SendableChooser<>();
-        }
-        RobotContainer.autoChooser.setDefaultOption("Drive Motor", "Drive Motor");
-        RobotContainer.autoMode.put("Drive Motor", new DriveMotor());
-        SmartDashboard.putData(RobotContainer.autoChooser);
-    }
+    }                   
 
     @Override
     public void robotPeriodic() {
-        edu.wpi.first.wpilibj2.command.CommandScheduler.getInstance().run();
         CommandScheduler.getInstance().run();
-
     }
 
     @Override
     public void autonomousInit() {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-        if (m_autonomousCommand != null) m_autonomousCommand.schedule();
+
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
+        }
     }
 
-    @Override public void autonomousPeriodic() {}
-    @Override public void teleopInit() {
-        if (m_autonomousCommand != null) m_autonomousCommand.cancel();
+    @Override
+    public void disabledInit() {
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
     }
-    @Override public void teleopPeriodic() {}
-    @Override public void disabledInit() {}
-    @Override public void disabledPeriodic() {}
-    @Override public void testInit() { CommandScheduler.getInstance().cancelAll(); }
-    @Override public void testPeriodic() {}
+
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,43 +1,24 @@
-
 package frc.robot;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.commands.Teleop;
-import frc.robot.commands.auto.DriveMotor;
-import frc.robot.gamepad.OI;
+import frc.robot.commands.DriveMotor;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ObjectDetector;
 
-import frc.robot.subsystems.VisionSubsystem;
-  
-public class RobotContainer
-{
+public class RobotContainer {
+    public static ExampleSubsystem o_subsystem;
+    public static ObjectDetector o_detector;
+    private final DriveMotor m_autoCommand;
 
-  public static ExampleSubsystem o_subsystem;
-  public static VisionSubsystem visionSubsystem;
-  public static OI oi;
+    public RobotContainer() {
 
-  public static SendableChooser<String> autoChooser;
-  public static Map<String, CommandBase> autoMode = new HashMap<>();
+        o_subsystem = new ExampleSubsystem();
+        o_detector = new ObjectDetector("10.0.2.2", 5800);
+        m_autoCommand = new DriveMotor(o_subsystem, o_detector);
+      
+    }
 
-  public RobotContainer()
-  {
-    o_subsystem = new ExampleSubsystem();
-    visionSubsystem = new VisionSubsystem();
-    oi = new OI();
-
-    o_subsystem.setDefaultCommand(new Teleop());
-  }
-
-  public Command getAutonomousCommand()
-  {
-    String mode = RobotContainer.autoChooser.getSelected();
-    SmartDashboard.putString("Chosen Auto Mode", mode);
-    return autoMode.getOrDefault(mode, new DriveMotor());
-  }
+    public Command getAutonomousCommand() {
+        return m_autoCommand;
+    }
 }
